45e930ecbfc003a9ab4e33a8e847c23498585e67
Rahul-ai <rt878777@gmail.com>
2022-06-30 13:23:42 +0530

    SetUp Strapi Project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd2035407dbc37a692e33ddb7ab85dc42'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '83d379b6-4d8f-47ea-bb1d-e85c8ca10d6f'
+};
